cfdc816d47f40151fc4db944eea1783a818f3ad5
mmkanta <47110972+mmkanta@users.noreply.github.com>
2022-06-29 18:06:31 +0700

    fix model

--- model/image/gradcam_generator.py
+++ model/image/gradcam_generator.py
@@ -1,7 +1,11 @@
 import numpy as np
 import tensorflow as tf
+#tf.compat.v1.enable_eager_execution()
+# tf.compat.v1.disable_eager_execution()
 from tensorflow.keras.utils import load_img, img_to_array, array_to_img
 import matplotlib.cm as cm
+# import cv2
+from tensorflow.keras import backend as K
 
 # https://keras.io/examples/vision/grad_cam/
 def make_gradcam_heatmap(img_array, model, last_conv_layer_name, pred_index=None):
@@ -69,3 +73,61 @@ def save_gradcam(img_path, heatmap, image_size, cam_path, alpha=0.4):
     # Save the superimposed image
     superimposed_img.save(cam_path)
     return cam_path
+
+# def grad_cam(input_model, image, cls, layer_name, image_size):
+#     """GradCAM method for visualizing input saliency."""
+#     y_c = input_model.output[0, cls]
+#     conv_output = input_model.get_layer(layer_name).output
+#     grads = K.gradients(y_c, conv_output)[0]
+#     #normalize if necessary
+#     gradient_function = K.function([input_model.input], [conv_output, grads])
+
+#     output, grads_val = gradient_function([image])
+#     output, grads_val = output[0, :], grads_val[0, :, :, :]
+
+#     weights = np.mean(grads_val, axis=(0, 1))
+#     cam = np.dot(output, weights)
+#     # print(cam)
+
+#     #process CAM
+#     cam = cv2.resize(cam, (image_size, image_size), cv2.INTER_LINEAR)
+#     cam = np.maximum(cam, 0)
+#     cam = cam / cam.max()
+#     return cam
+
+# def compute_saliency(model, img_path, image_size, cam_path, layer_name='top_activation', cls=-1):
+#     """Compute saliency using all three approaches.
+#         -layer_name: layer to compute gradients;
+#         -cls: class number to localize (-1 for most probable class).
+#     """
+
+#     img = load_img(
+#         img_path,
+#         color_mode='rgb',
+#         target_size=(image_size, image_size)
+#     )
+#     preprocessed_input = np.expand_dims(img_to_array(img)/255, axis=0)
+# #     print(preprocessed_input)
+
+#     predictions = model.predict(preprocessed_input)
+#     # print(predictions)
+# #     top = decode_predictions(predictions, top=top_n)[0]
+# #     classes = np.argsort(predictions[0])[-top_n:][::-1]
+# #     print('Model prediction:')
+# #     for c, p in zip(classes, top):
+# #         print('\t{:15s}\t({})\twith probability {:.3f}'.format(p[0], c, p[1]))
+#     if cls == -1:
+#         cls = np.argmax(predictions)
+#     # print(cls)
+# #     class_name = decode_predictions(np.eye(1, 2, cls))[0][0][0]
+# #     print("Explanation for '{}'".format(class_name))
+    
+#     gradcam = grad_cam(model, preprocessed_input, cls, layer_name, image_size)
+#     # print(gradcam)
+
+#     jetcam = cv2.applyColorMap(np.uint8(255 * gradcam), cv2.COLORMAP_JET)
+#     # print(img)
+#     jetcam = (np.float32(jetcam) + img) / 2
+#     cv2.imwrite(cam_path, np.uint8(jetcam))
+        
+#     return predictions[0][1]
